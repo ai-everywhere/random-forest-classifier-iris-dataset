d13bab44a9a4741aff7e8cab004b24d0be4826a2
Derroh Amany Maxime Konan <derrohmaxime@gmail.com>
2020-07-27 16:21:45 +0530

    Adding prediction model and api

---
+++ flask_predict_train.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 13 12:02:27 2018
+
+Updated on Mon Jul 27 16:14:13 2020
+
+@author: derrohmaxime
+"""
+
+# Random Forest Classifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import pandas as pd
+
+# loading the dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+# Split the data
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.5)
+
+# Build the model
+clf = RandomForestClassifier(n_estimators=10)
+
+# Train the classifier
+clf.fit(X_train, y_train)
+
+# Predictions
+predicted = clf.predict(X_test)
+
+# Check accuracy 
+print(accuracy_score(predicted, y_test))
+
+import pickle
+with open('../random-forest-classifier-iris-dataset/rf.pkl', 'wb') as model_pkl:
+    pickle.dump(clf, model_pkl, protocol=2)
+    
